cc61a152419ea4d4f4708b496e8114b9ffa1098e
kaorukoyama <rufii72865@gmail.com>
2024-02-26 07:48:42 +0900

    スプレッド記法や配列、オブジェクトの記述における情報の渡り方を学習

--- 01_js_basic/060_spread_rest/start/main.js
+++ 01_js_basic/060_spread_rest/start/main.js
@@ -1,25 +1,25 @@
 const nums = [3, 1, 4, 1, 5, 10, 2, 6];
 
-const result = Math.max(3, 1, 4, 1, 5, 10, 2, 6);
+const result = Math.max(...nums);
 console.log(result);
-
 
 let arr1 = [1, 2, 3];
 let arr2 = [4, 5, 6];
-let newArr = [...arr1];
+let newArr = [...arr1, 10, ...arr2];
+let newArr1 = arr1;
 
-console.log(newArr2);
+console.log(newArr);
 
 const obj = {
   name: "Tom",
   age: 22,
 };
 const newObj = { ...obj };
-
-console.log(newObj);
+newObj.name = "john";
+console.log(newObj, obj);
 
 const restA = (...argA) => console.log(argA);
-restA(1, 3, 4)
+restA(1, 3)
 
 const restB = (n, ...argB) => console.log(argB);
 restB(1, 3, 4)
