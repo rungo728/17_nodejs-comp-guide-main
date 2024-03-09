6499f37ab913be43f92392ee05583dd83acf0f03
kaorukoyama <rufii72865@gmail.com>
2024-03-09 15:24:56 +0900

    非同期処理の書き方などについて学習

--- 02_node_basic/030_write_file/start/example.js
+++ 02_node_basic/030_write_file/start/example.js
@@ -1,1 +1,6 @@
-console.log('hello, node.js');
+const fs = require("fs");
+const path = require("path");
+
+console.log(__filename);
+fs.writeFileSync(__dirname + "../dist/test.txt", "hello, node.js");
+// console.log("hello, node.js");
